0bab8af4562f8659a8927ba8add31e8872bd48f7
Vano2903 <davidevanoncini2003@gmail.com>
2022-03-23 18:19:09 +0100

    working on methods of oauther, generating tokens,

--- oauth.go
+++ oauth.go
@@ -18,6 +18,13 @@ import (
 	"google.golang.org/api/option"
 )
 
+type Token struct {
+	AccessToken            string
+	ExpirationAccessToken  time.Time
+	RefreshToken           string
+	ExpirationRefreshToken time.Time
+}
+
 /*
 functions:
 =====OAUTH
@@ -38,7 +45,7 @@ type Oauther struct {
 	OauthService *oauth.Service
 	UserInfo     *oauth.Userinfoplus
 	OauthCode    string
-	ServerToken  *oauth2.Token
+	ServerToken  *Token
 	//db connection
 }
 
@@ -70,15 +77,17 @@ func (o *Oauther) GenerateOauthUrl(conn *sql.Conn) (string, error) {
 		rand.Read(b)
 		state = fmt.Sprintf("%x", b)
 		//1 minute long expiration timestamp
-		expriation = time.Now().Add(time.Minute * 1)
+		expriation = time.Now().Add(time.Minute * 5)
 		var res string
+		var t time.Time
 		//check if it's in the db already
-		conn.QueryRowContext(o.Ctx, "SELECT * FROM oauth WHERE state = ?", state).Scan(res)
+		conn.QueryRowContext(o.Ctx, "SELECT * FROM oauth WHERE state = ?", state).Scan(res, t)
 		if res == "" {
 			//if not, break the loop
 			break
 		}
 	}
+
 	//insert the state and expiration into the db
 	_, err := conn.ExecContext(o.Ctx, "INSERT INTO states (value, expiration) VALUES (?, ?)", state, expriation)
 	if err != nil {
@@ -89,15 +98,18 @@ func (o *Oauther) GenerateOauthUrl(conn *sql.Conn) (string, error) {
 }
 
 //get the oauth code from the url
-func (o *Oauther) GetOauthCodeFromUrl(conn *sql.Conn, oauthUrl string) (string, error) {
+func (o *Oauther) GetOauthCodeFromUrl(conn *sql.Conn, oauthUrl string) error {
+	//decode the url encoded url string
+	//%2F => /
 	oauthUrlDecoded, err := url.QueryUnescape(oauthUrl)
 	if err != nil {
-		return "", err
+		return err
 	}
 
+	//convert the url parameters to a map
 	vals, err := url.ParseQuery(oauthUrlDecoded)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	state := vals.Get("state")
@@ -107,21 +119,21 @@ func (o *Oauther) GetOauthCodeFromUrl(conn *sql.Conn, oauthUrl string) (string, 
 	var stateFromDB string
 	err = conn.QueryRowContext(o.Ctx, "SELECT * FROM states WHERE value = ?", state).Scan(&stateFromDB, &expriation)
 	if err != nil {
-		return "", err //state not found
+		return err //state not found
 	}
 
 	//delete the state from the db
 	_, err = conn.ExecContext(o.Ctx, "DELETE FROM states WHERE value = ?", state)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	//check if the state is expired
 	if time.Now().After(expriation) {
-		return "", fmt.Errorf("the state is expired")
+		return fmt.Errorf("the state is expired")
 	}
 	o.OauthCode = vals.Get("code")
-	return vals.Get("code"), nil
+	return nil
 }
 
 //get the token pair from the oauth code
@@ -156,13 +168,72 @@ func (o *Oauther) GetUserInformation() error {
 	return nil
 }
 
-//! on development
-// func (o *Oauther) GenerateTokensFromUserEmail(conn *sql.Conn) error {
-// 	email := o.UserInfo.Email
-// 	//generate the access and refresh token
-// 	var accessToken, refreshToken string
+//generate a token pair 64 byte long access and refresh token
+//the access token last for 15 minute while the refresh token 1 week
+//both of the token are stored in the database, to call this function the
+//UserInfo field must not be nil
+func (o *Oauther) GenerateTokensFromUserEmail(connection *sql.DB) error {
+	//generate a random 64 character string and check that's not in the db
+	//generate the access token and it expires in 15 minutes
+	for {
+		//create a 64 byte random string
+		b := make([]byte, 32)
+		rand.Read(b)
+		o.ServerToken.AccessToken = fmt.Sprintf("%x", b)
+		o.ServerToken.ExpirationAccessToken = time.Now().Add(time.Minute * 15)
+		//check if the access token is already in the database
+		found, err := tokenExists(o.ServerToken.AccessToken, connection)
+		if err != nil {
+			return err
+		}
 
-// }
+		if !found {
+			break
+		}
+	}
+
+	//same thing of the access token but for the refresh token, the expiration time is longer (7 days)
+	for {
+		//create a 64 byte random string
+		b := make([]byte, 32)
+		rand.Read(b)
+		o.ServerToken.RefreshToken = fmt.Sprintf("%x", b)
+		o.ServerToken.ExpirationRefreshToken = time.Now().Add(time.Hour * 24 * 7)
+		//check if the access token is already in the database
+		found, err := tokenExists(o.ServerToken.AccessToken, connection)
+		if err != nil {
+			return err
+		}
+
+		if !found {
+			break
+		}
+	}
+
+	//insert the token pair into the database
+	accessTokenQuery := "INSERT INTO accessTokens (accToken, accExp) VALUES (?, ?)"
+	refreshTokenQuery := "INSERT INTO refreshTokens (refreshToken, refreshExp) VALUES (?, ?)"
+
+	accessResult, err := connection.Exec(accessTokenQuery, o.ServerToken.AccessToken, o.ServerToken.ExpirationAccessToken)
+	if err != nil {
+		return err
+	}
+
+	refreshResult, err := connection.Exec(refreshTokenQuery, o.ServerToken.RefreshToken, o.ServerToken.ExpirationRefreshToken)
+	if err != nil {
+		return err
+	}
+
+	//get the ids of the tokens just inserted
+	accessID, _ := accessResult.LastInsertId()
+	refreshID, _ := refreshResult.LastInsertId()
+
+	//relationship between the tokens and the email
+	relationShipQuery := "INSERT INTO tokens (email, accID, refID) VALUES (?, ?, ?)"
+	_, err = connection.Exec(relationShipQuery, o.UserInfo.Email, accessID, refreshID)
+
+	return err
+}
 
 //generate a new oauther struct
 //the config file is the google api config file (client id, client secret, redirect url, check out the clientgoogle.json.example file)
@@ -188,4 +259,21 @@ func NewOauther(credentialFilePath string) (*Oauther, error) {
 		Ctx:    context.Background(),
 		Config: config,
 	}, nil
+}
+
+//check if a token is already in the database (either access or refresh token)
+func tokenExists(token string, connection *sql.DB) (bool, error) {
+	query := "SELECT * FROM tokens t JOIN accessTokens a ON a.ID = t.accID JOIN refreshTokens r ON r.ID = t.refID WHERE a.accToken = ? OR r.refreshToken = ?"
+
+	var found bool
+	err := connection.QueryRow(query, token, token).Scan(&found)
+	if err != nil {
+		//if the error is ErrNoRows then the token is not in the database
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return true, nil
 }
