da1dee245fc60c17c2cf0ba60911ff02ae446775
MoraGames <andreamorandi2612@gmail.com>
2022-03-26 18:03:10 +0100

    Added front-end template and CalendarManager_ProjectDraft file.

---
+++ statics/endpoints.js
@@ -0,0 +1,5 @@
+// List of all endpoints
+const endpoints = new Map();
+endpoints.set("loginPage", "endpointUrl");
+endpoints.set("homePage", "endpointUrl");
+endpoints.set("newCalendar", "endpointUrl");
