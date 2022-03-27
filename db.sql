6470466287df4e2c01c0eca44f885f5abf7d1b4f
davide vanoncini <davidevanoncini2003@gmail.com>
2022-03-27 22:41:19 +0200

    added tables and corrected sql code

--- db.sql
+++ db.sql
@@ -1,6 +1,36 @@
-CREATE TABLE IF NOT EXISTS states{
-    value VARCHAR(20) PRIMARY KEY,
-    expiration TIMESTAMP,
-    PRIMARY KEY (value),
-};
+CREATE TABLE IF NOT EXISTS `states` (
+    `value` VARCHAR(20) NOT NULL,
+    `expiration` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `tokens` (
+    `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `email` varchar(75) not null,
+    `accID` int NOT NULL,
+    `refID` int NOT NULL,
+    `googleID` int NOT NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `accessTokens` (
+    `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `accToken` char(64) NOT NULL,
+    `accExp` DATETIME NOT NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `refreshTokens` (
+    `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `refreshToken` char(64) NOT NULL,
+    `refreshExp` DATETIME NOT NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `googleTokens` (
+    `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `googleAccessToken` text NOT NULL,
+    `googleExp` DATETIME NOT NULL,
+    `googleRefreshToken` text NOT NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
